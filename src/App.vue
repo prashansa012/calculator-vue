9c5e810a9b15c8d5d4c8e52c3ed8347a98788231
prashansa012 <156915156+prashansa012@users.noreply.github.com>
2024-02-09 19:04:18 +0530

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+ <div id="app">
+  <h1></h1>
+  <calculater/>
+ </div>
+</template>
+
+<script>
+import Calculater from './components/Calculater.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculater
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
